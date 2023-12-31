405d757e7c5de546b7767cea4acdab8ba7c73808
Ahmed Awad <awad55397@gmail.com>
2023-12-31 17:11:20 +0100

    CRUD Managment System

---
+++ app.js
@@ -0,0 +1,7 @@
+// // import { myAge, arr, saySomething as s } from "./main.js";
+// import * as all from "./main.js";
+// console.log(all);
+// console.log(all.myAge);
+export default function (one, two, three) {
+  return one + two + three;
+}
